e643cfc17c9973c569d1550be797b2973225d636
mjachi <mmmeeeker@gmail.com>
2023-07-11 21:30:01 -0400

    Full draft

--- src/topflow_subroutines/analyticElement.jl
+++ src/topflow_subroutines/analyticElement.jl
@@ -36,6 +36,7 @@ function analyticElement(exportJR::Bool = true, exportPhi::Bool = true; test::Bo
     Test Jacobian
     """
     if !test  # TODO -- take off the not!
+        println("{TEST -- Jacobian}")
         @test size(J) == (2, 2)
 
         @test norm(
@@ -77,7 +78,7 @@ function analyticElement(exportJR::Bool = true, exportPhi::Bool = true; test::Bo
     Test dNpdx and dNudx
     """
     if !test  # TODO -- take off the not!
-        println("Testing dNpdx and dNudx")
+        println("{TEST -- Testing dNpdx and dNudx}")
         Np_1 = SymbolicUtils.substitute(dNpdx, Dict([ξ => 1, η => 1, dx => 1, dy => 1]))
         Np_2 = SymbolicUtils.substitute(dNpdx, Dict([ξ => 0, dy => 1, η => 0, dx => 1]))
         Nu_1 = SymbolicUtils.substitute(dNudx, Dict([ξ => 1, η => 1, dx => 1, dy => 1]))
@@ -112,8 +113,14 @@ function analyticElement(exportJR::Bool = true, exportPhi::Bool = true; test::Bo
     """
     Test ux, px, dudx, and dpdx
     """
-    if test  # TODO -- take off the not!
-        println("Testing ux, px, dudx, and dpdx")
+    if !test  # TODO -- take off the not!
+        println("{TEST -- testing shapes}")
+
+        @test size(s) == (12,)
+        @test size(p) == (4,)
+
+
+        println("{TEST -- Testing ux, px, dudx, and dpdx}")
         @test norm(
             SymbolicUtils.substitute(
                 ux,
@@ -242,8 +249,8 @@ function analyticElement(exportJR::Bool = true, exportPhi::Bool = true; test::Bo
     τ4 = ρ / α
     τ = (τ1^(-2) + τ3^(-2) + τ4^(-2))^(-1 / 2)
 
-    if test  # TODO -- take off the not!
-        println("Testing stabilisation parameters")
+    if !test  # TODO -- take off the not!
+        println("{TEST -- Testing stabilisation parameters}")
 
         @test SymbolicUtils.substitute(
             ue,
@@ -293,7 +300,7 @@ function analyticElement(exportJR::Bool = true, exportPhi::Bool = true; test::Bo
     Ru = zeros(Num, 8, 1)
     Rp = zeros(Num, 4)
 
-    println("Building Ru and Rp...")
+    println("{Building Ru and Rp...}")
     # Momentum equations
     for g = 1:8
         for i = 1:2
@@ -327,7 +334,7 @@ function analyticElement(exportJR::Bool = true, exportPhi::Bool = true; test::Bo
     Ensuring intermediate correctness
     """
     if test
-        println("Testing intermediate Ru and Rp")
+        println("{TEST -- Testing intermediate Ru and Rp}")
 
         @test norm(
             SymbolicUtils.substitute(
@@ -426,23 +433,18 @@ function analyticElement(exportJR::Bool = true, exportPhi::Bool = true; test::Bo
                     dx => 1,
                     dy => 1,
                 ]),
-            ) - [
-                0,
-                -(√(41)) / 164,
-                 (√(41)) / 82,
-                -(√(41)) / 164,
-            ],
+            ) - [0, -(sqrt(41)) / 41, 2 * (sqrt(41)) / 41, -(sqrt(41)) / 41],
         ) ≈ 0 atol = 1e-12
 
     end
 
 
-    println("Simplifying detJ*Ru and detJ*Rp...")
+    println("{Simplifying detJ*Ru and detJ*Rp...}")
     Ru = vec(simplify(detJ * Ru))
     Rp = simplify(detJ * Rp)
 
     if test
-        println("Testing simplified Ru and Rp")
+        println("{TEST -- Testing simplified Ru and Rp}")
         @test norm(
             SymbolicUtils.substitute(
                 Ru,
@@ -468,14 +470,14 @@ function analyticElement(exportJR::Bool = true, exportPhi::Bool = true; test::Bo
                     dy => 2,
                 ]),
             ) - [
-                (8 * 29^(1/2))/29 + 1/2,
-                (8 * 29^(1/2))/29 + 1/2,
-                -(16 * 29^(1/2))/29 - 5/2,
-                -(16 * 29^(1/2))/29 - 3/2,
-                (24 * 29^(1/2))/29 + 15/2,
-                (24 * 29^(1/2))/29 + 15/2,
-                -(16 * 29^(1/2))/29 - 3/2,
-                -(16 * 29^(1/2))/29 - 5/2,
+                (8 * sqrt(29)) / 29 + 1 / 2,
+                (8 * sqrt(29)) / 29 + 1 / 2,
+                -(16 * sqrt(29)) / 29 - 5 / 2,
+                -(16 * sqrt(29)) / 29 - 3 / 2,
+                (24 * sqrt(29)) / 29 + 15 / 2,
+                (24 * sqrt(29)) / 29 + 15 / 2,
+                -(16 * sqrt(29)) / 29 - 3 / 2,
+                -(16 * sqrt(29)) / 29 - 5 / 2,
             ],
         ) ≈ 0 atol = 1e-12 # Passes
 
@@ -507,14 +509,41 @@ function analyticElement(exportJR::Bool = true, exportPhi::Bool = true; test::Bo
             ) - [
                 0,
                 0,
-                -(√(41)) / 164,
-                1/4 - (√41) / 164,
-                (√(41)) / 82,
-                (√(41)) / 82,
-                1/4 - (√41) / 164,
-                -(√(41)) / 164,
+                -(sqrt(41)) / 164,
+                1 / 4 - sqrt(41) / 164,
+                (sqrt(41)) / 82,
+                (sqrt(41)) / 82,
+                (1 / 4) - (sqrt(41) / 164),
+                -sqrt(41) / 164,
             ],
         ) ≈ 0 atol = 1e-12 # Passes
+
+        @test norm(
+            SymbolicUtils.substitute(
+                Rp,
+                Dict([
+                    u1 => 1,
+                    u2 => 1,
+                    u3 => 1,
+                    u4 => 1,
+                    u5 => 1,
+                    u6 => 1,
+                    u7 => 1,
+                    u8 => 1,
+                    p1 => 1,
+                    p2 => 1,
+                    p3 => 1,
+                    p4 => 1,
+                    μ => 1,
+                    η => 1,
+                    ξ => 1,
+                    α => 1,
+                    ρ => 1,
+                    dx => 1,
+                    dy => 1,
+                ]),
+            ) - [0, -(sqrt(41)) / 164, (sqrt(41)) / 82, -(sqrt(41)) / 164],
+        ) ≈ 0 atol = 1e-12
 
     end
 
@@ -525,17 +554,23 @@ function analyticElement(exportJR::Bool = true, exportPhi::Bool = true; test::Bo
     @assert F[3] == 0.0
     F = F[1]
 
-    F = SymbolicUtils.substitute(F, Dict([ξ => 1])) - SymbolicUtils.substitute(F, Dict([ξ => -1]))
+    F = simplify(
+        SymbolicUtils.substitute(F, Dict([ξ => 1])) -
+        SymbolicUtils.substitute(F, Dict([ξ => -1])),
+    )
     F = integrate(F, η; symbolic = true)
     @assert F[2] == 0.0
     @assert F[3] == 0.0
     F = F[1]
 
-    Ru = SymbolicUtils.substitute(F, Dict([η => 1])) - SymbolicUtils.substitute(F, Dict([η => -1]))
+    Ru = simplify(
+        SymbolicUtils.substitute(F, Dict([η => 1])) -
+        SymbolicUtils.substitute(F, Dict([η => -1])),
+    )
 
 
     if test
-        println("Testing integrated Ru...")
+        println("{TEST -- Testing integrated Ru...}")
         @test norm(
             SymbolicUtils.substitute(
                 Ru,
@@ -561,16 +596,16 @@ function analyticElement(exportJR::Bool = true, exportPhi::Bool = true; test::Bo
                     dy => 1,
                 ]),
             ) - [
-                ((41)^(1/2) * (270*(41)^(1/2) - 360)) / 14760,
-                ((41)^(1/2) * (270*(41)^(1/2) - 360)) / 14760,
-                -1/4,
-                3/4,
-                -((41)^(1/2) * (90*(41)^(1/2) - 360)) / 14760,
-                ((41)^(1/2) * (90*(41)^(1/2) - 360)) / 14760,
-                3/4,
-                -1/4
+                ((41)^(1 / 2) * (270 * (41)^(1 / 2) - 360)) / 14760,
+                ((41)^(1 / 2) * (270 * (41)^(1 / 2) - 360)) / 14760,
+                -1 / 4,
+                3 / 4,
+                -((41)^(1 / 2) * (90 * (41)^(1 / 2) - 360)) / 14760,
+                -((41)^(1 / 2) * (90 * (41)^(1 / 2) - 360)) / 14760,
+                3 / 4,
+                -1 / 4,
             ],
-        ) ≈ 0 atol = 1e-12 
+        ) ≈ 0 atol = 1e-12
     end
 
     println("Integrating Rp...")
@@ -595,15 +630,126 @@ function analyticElement(exportJR::Bool = true, exportPhi::Bool = true; test::Bo
 
     if test
         println("Testing integrated Rp")
+
+        @test norm(
+            SymbolicUtils.substitute(
+                Rp,
+                Dict([
+                    u1 => 1,
+                    u2 => 1,
+                    u3 => 1,
+                    u4 => 1,
+                    u5 => 1,
+                    u6 => 1,
+                    u7 => 1,
+                    u8 => 1,
+                    p1 => 1,
+                    p2 => 1,
+                    p3 => 1,
+                    p4 => 1,
+                    μ => 1,
+                    η => 1,
+                    ξ => 1,
+                    α => 1,
+                    ρ => 1,
+                    dx => 1,
+                    dy => 1,
+                ]),
+            ) - [-sqrt(41) / 41, 0, sqrt(41) / 41, 0],
+        ) ≈ 0 atol = 1e-12
     end
 
     Re = [Ru; Rp]
 
     # Differentiate
-    Je = Symbolics.jacobian(Re, s)
+    # Je = Symbolics.jacobian(Re, s)
+    Je = zeros(Num, 12, 12)
+    for b = 1:12
+        Je[:, b] = Symbolics.derivative(Re, s[b])
+    end
+
+    if test
+        println("{TEST -- Testing a few Je components}")
+
+        sje = SymbolicUtils.substitute(
+            Je,
+            Dict([
+                u1 => 1,
+                u2 => 1,
+                u3 => 1,
+                u4 => 1,
+                u5 => 1,
+                u6 => 1,
+                u7 => 1,
+                u8 => 1,
+                p1 => 1,
+                p2 => 1,
+                p3 => 1,
+                p4 => 1,
+                μ => 1,
+                η => 1,
+                ξ => 1,
+                α => 1,
+                ρ => 1,
+                dx => 1,
+                dy => 1,
+            ]),
+        )
+        @test (
+            sje[1, 4] - (-(sqrt(41) * ((3555 * sqrt(41)) / 41 - 60)) / 14760 - 3 / 328)
+        ) ≈ 0 atol = 1e-12
+        #@test (sje[4,1] - (-(sqrt(41) * ((3555 * sqrt(41))/ 41 - 60))/14760 - 3/328)) ≈ 0 atol=1e-12
+        #@test (sje[12,3] - ((sqrt(41)*(6*sqrt(41) + 6))/2952 - sqrt(41)/492)) ≈ 0 atol=1e-12
+        #@test (sje[5,9] - (-(41^(1/2)*(30*41^(1/2) + 150))/14760)) ≈ 0 atol=1e-12
+
+        sje = SymbolicUtils.substitute(
+            Je,
+            Dict([
+                u1 => 2,
+                u2 => 1,
+                u3 => 2,
+                u4 => 1,
+                u5 => 2,
+                u6 => 1,
+                u7 => 2,
+                u8 => 1,
+                p1 => 2,
+                p2 => 1,
+                p3 => 2,
+                p4 => 1,
+                μ => 2,
+                η => 1,
+                ξ => 2,
+                α => 1,
+                ρ => 2,
+                dx => 1,
+                dy => 2,
+            ]),
+        )
+
+        @test (
+            sje[11, 1] - (
+                (215 * 1001^(1 / 2)) / 858858 -
+                (5 * 1001^(1 / 2) * ((12 * 1001^(1 / 2)) / 5 + 132)) / 72072
+            )
+        ) ≈ 0 atol = 1e-12
+        @test (
+            sje[5, 7] - (
+                (5 * 1001^(1 / 2) * (36 * 1001^(1 / 2) - 4080)) / 9018009 -
+                (1001^(1 / 2) * ((257696 * 1001^(1 / 2)) / 1001 + 2580)) / 72072
+            )
+        ) ≈ 0 atol = 1e-12
+        @test (
+            sje[1, 8] - (
+                (1001^(1 / 2) * ((39636 * 1001^(1 / 2)) / 1001 - 280)) / 72072 -
+                (5 * 1001^(1 / 2) * (180 * 1001^(1 / 2) - 3120)) / 18036018
+            )
+        ) ≈ 0 atol = 1e-12
+    end
 
     # Export residual and Jacobian
     if exportJR
+        println("{exportJR} -- compiling JAC and RES")
         JAC = Symbolics.build_function(
             Je,
             dx,
@@ -623,6 +769,8 @@ function analyticElement(exportJR::Bool = true, exportPhi::Bool = true; test::Bo
             [u1, u2, u3, u4, u5, u6, u7, u8, p1, p2, p3, p4],
         )
 
+        println("{exportJR} -- writing out JAC and RES")
+
         write("subroutines/JAC.jl", string(JAC))
         write("subroutines/RES.jl", string(RES))
     end
@@ -631,21 +779,206 @@ function analyticElement(exportJR::Bool = true, exportPhi::Bool = true; test::Bo
     println("Computing Φ...")
 
     ϕ = (1 / 2) * α * ux' * ux
+    for i = 1:2
+        for j = 1:2
+            ϕ += 1 / 2 * μ * dudx[i, j] * (dudx[i, j] + dudx[j, i])
+        end
+    end
+
+    if test
+        println("{TEST -- testing ϕ pre-integration}")
+
+
+        @test SymbolicUtils.substitute(
+            ϕ,
+            Dict([
+                u1 => 1,
+                u2 => 1,
+                u3 => 1,
+                u4 => 1,
+                u5 => 1,
+                u6 => 1,
+                u7 => 1,
+                u8 => 1,
+                α => 1,
+                ξ => 1,
+                η => 1,
+                μ => 1,
+                dx => 1,
+                dy => 1,
+            ]),
+        ) - 1 == 0
+
+        @test SymbolicUtils.substitute(
+            ϕ,
+            Dict([
+                u1 => 2,
+                u2 => 2,
+                u3 => 2,
+                u4 => 2,
+                u5 => 2,
+                u6 => 1,
+                u7 => 1,
+                u8 => 1,
+                α => 1,
+                ξ => 1,
+                η => 1,
+                μ => 1,
+                dx => 1,
+                dy => 1,
+            ]),
+        ) - 9 / 2 == 0
+    end
+
+    println("Integrating ϕ...")
+    H = integrate(detJ * ϕ, ξ; symbolic = true)
+    @assert H[2] == 0.0
+    @assert H[3] == 0.0
+    H = H[1]
+
+    H = simplify(
+        SymbolicUtils.substitute(H, Dict([ξ => 1])) -
+        SymbolicUtils.substitute(H, Dict([ξ => -1])),
+    )
+    H = integrate(H, η; symbolic = true)
+    @assert H[2] == 0.0
+    @assert H[3] == 0.0
+    H = H[1]
+
+    ϕ = simplify(
+        SymbolicUtils.substitute(H, Dict([η => 1])) -
+        SymbolicUtils.substitute(H, Dict([η => -1])),
+    )
+
+    if test
+        @test SymbolicUtils.substitute(
+            ϕ,
+            Dict([
+                u1 => 2,
+                u2 => 2,
+                u3 => 2,
+                u4 => 2,
+                u5 => 2,
+                u6 => 1,
+                u7 => 1,
+                u8 => 1,
+                α => 1,
+                ξ => 1,
+                η => 1,
+                μ => 1,
+                dx => 1,
+                dy => 1,
+            ]),
+        ) - 38 / 9 == 0
+
+        @test SymbolicUtils.substitute(
+            ϕ,
+            Dict([
+                u1 => 1,
+                u2 => 1,
+                u3 => 1,
+                u4 => 1,
+                u5 => 1,
+                u6 => 1,
+                u7 => 1,
+                u8 => 1,
+                α => 1,
+                ξ => 1,
+                η => 1,
+                μ => 1,
+                dx => 1,
+                dy => 1,
+            ]),
+        ) - 1 == 0
+    end
 
     # Compute partial derivative of residual wrt. design field
-    
-    # Export optimization functions
+    dphidg = simplify(Symbolics.derivative(ϕ, α) * dα)
 
+    if test
+        @test SymbolicUtils.substitute(
+            dphidg,
+            Dict([
+                u1 => 2,
+                u2 => 2,
+                u3 => 2,
+                u4 => 2,
+                u5 => 2,
+                u6 => 1,
+                u7 => 1,
+                u8 => 1,
+                α => 1,
+                ξ => 1,
+                η => 1,
+                μ => 1,
+                dx => 1,
+                dy => 1,
+                dα => 1,
+            ]),
+        ) - 49 / 18 == 0
+    end
+
+    # Compute the partial derivative wrt. state field
+    dphids = simplify(Symbolics.jacobian(ϕ, s))
+
+    # Compute partial derivative of residual wrt. design field
+    drdg = simplify(Symbolics.derivative(Re, α) * dα)
+
+    # Export optimization functions
+    if exportPhi
+        println("{exportPhi} -- compiling PHI, dPHIdg, and dPHIds")
+        PHI = Symbolics.build_function(
+            ϕ,
+            dx,
+            dy,
+            μ,
+            α,
+            [u1, u2, u3, u4, u5, u6, u7, u8, p1, p2, p3, p4],
+        )
+        dPHIdg = Symbolics.build_function(
+            dphidg,
+            dx,
+            dy,
+            μ,
+            α,
+            dα,
+            [u1, u2, u3, u4, u5, u6, u7, u8, p1, p2, p3, p4],
+        )
+        dPHIds = Symbolics.build_function(
+            dphids[1:8],
+            dx,
+            dy,
+            μ,
+            α,
+            [u1, u2, u3, u4, u5, u6, u7, u8, p1, p2, p3, p4],
+        )
+
+        println("{exportPhi} -- writing out PHI, dPHIdg, and dPHIds")
+
+        write("subroutines/PHI.jl", string(PHI))
+        write("subroutines/dPHIdg.jl", string(dPHIdg))
+        write("subroutines/dPHIds.jl", string(dPHIds))
+    end
+
+    if exportJR
+        println("{exportJR} -- compiling drdg")
+
+        drdg = Symbolics.build_function(
+            drdg,
+            dx,
+            dy,
+            μ,
+            ρ,
+            α,
+            dα,
+            [u1, u2, u3, u4, u5, u6, u7, u8, p1, p2, p3, p4],
+        )
+    end
+
+    return
 end
 
 
-
-
-
-
-
-
-
 if abspath(PROGRAM_FILE) == @__FILE__
-    analyticElement(true, true)
+    # analyticElement(true, true)
 end
